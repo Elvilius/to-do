86cc88bb250fac88f5a69abbb300281a25a8611f
sergienko <vik.sergienko@mail.ru>
2019-11-21 21:44:42 +0300

    Init base serve

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log('start');
+});

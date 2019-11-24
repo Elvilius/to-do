2748238a5acc0835f6d6f0e161ad4d0e1b9d4540
sergienko <vik.sergienko@mail.ru>
2019-11-24 22:46:13 +0300

    Added connection database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    url: process.env.DEV_DATABASE_URL,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
   },
   test: {

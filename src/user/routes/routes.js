bd9dd3c38cdb2f6a5dc36a056bed5f86d335419c
sergienko <vik.sergienko@mail.ru>
2019-11-25 14:46:22 +0300

    Fixed

--- src/user/routes/routes.js
+++ src/user/routes/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 // import validateUser from '../../middleware/validates';
-import model from '../../database/models/index';
+import    model from '../../database/models/index';
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-  res.send(model.User.findAll());
+router.get('/login', async (req, res) => {
+  const user = await model.User.findOne({ email: 'fnord', password: 'omnomnom', token: 'qweqweqweqwe' });
+  console.log(user);
+  res.send(user);
 });
 
 // router.post('/registration', validateUser, true);

ffc78b358dc1434f61719de963db1764cd8c1f32
sergienko <vik.sergienko@mail.ru>
2019-12-01 20:54:28 +0300

    Added local passport

--- src/auth/passport/local/passport.js
+++ src/auth/passport/local/passport.js
@@ -1,5 +1,20 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
+import { findUserByEmail } from '../../../user/handlers/user';
 
-passport.use('local', new LocalStrategy({}, (userName, password, done) => done(null, false)));
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+passport.use('local', new LocalStrategy({
+
+  usernameField: 'email',
+
+}, async (userName, password, done) => {
+  const user = await findUserByEmail(userName);
+  if (!user) {
+    return done(null, false, { message: 'Unauthorized' });
+  }
+  return done(null, user);
+}));
+
 export default passport;

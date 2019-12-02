e93321c02b591ee35e02f41b1df686ad6a820d18
sergienko <vik.sergienko@mail.ru>
2019-12-02 10:07:23 +0300

    Fixed user model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,12 +1,18 @@
+import * as bcrypt from 'bcrypt';
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    token: DataTypes.STRING,
+    email: [DataTypes.STRING, DataTypes.email],
   }, {});
 
   User.associate = () => {
     // associations can be defined here
   };
+  // eslint-disable-next-line func-names
+  User.prototype.validatePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+
   return User;
 };

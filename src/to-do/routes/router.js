739f422ad565f10eed85e09f290f24d9d2a5cb7c
sergienko <vik.sergienko@mail.ru>
2019-12-02 11:38:18 +0300

    Added passport jwt

--- src/to-do/routes/router.js
+++ src/to-do/routes/router.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { getList, createTask } from '../handlers/handler';
+import { getList, createTask } from '../handlers/to-do';
 import { createToDoValidate } from '../../middleware/validates';
+import passportJwt from '../../auth/passport/passport-jwt';
 
 const router = express.Router();
 
+router.use(passportJwt.authenticate('jwt'));
+
 router.get('/list', getList);
 router.post('/create', createToDoValidate, createTask);
+router.post('/updated/id', createToDoValidate, createTask);
+router.post('/set-status/id', createToDoValidate, createTask);
 
 
 export default router;

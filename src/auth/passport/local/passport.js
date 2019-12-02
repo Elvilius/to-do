98fe0e38441d3f625c3d45af7051df5f3644af43
sergienko <vik.sergienko@mail.ru>
2019-12-02 10:06:40 +0300

    Fixed passport local

--- src/auth/passport/local/passport.js
+++ src/auth/passport/local/passport.js
@@ -8,10 +8,11 @@ passport.deserializeUser((user, done) => done(null, user));
 passport.use('local', new LocalStrategy({
 
   usernameField: 'email',
+  passwordField: 'password',
 
 }, async (userName, password, done) => {
   const user = await findUserByEmail(userName);
-  if (!user) {
+  if (!user || !user.validatePassword(password)) {
     return done(null, false, { message: 'Unauthorized' });
   }
   return done(null, user);

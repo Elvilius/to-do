54f0efa061721cb7d20598b252f7673d95a4bf96
sergienko <vik.sergienko@mail.ru>
2019-12-16 22:06:42 +0300

    Fixed connect data base

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,13 +4,22 @@ module.exports = {
   development: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
